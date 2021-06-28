2db4d38788c524bcb5e94f0266ad1e917572f55e
anand564 <85823928+anand564@users.noreply.github.com>
2021-06-29 02:42:12 +0530

    my project

--- store/models/product.py
+++ store/models/product.py
@@ -9,7 +9,13 @@ class Product(models.Model):
     description = models.CharField(max_length=200, default='', null=True, blank=True)
     image = models.ImageField(upload_to='uploads/products/')
 
-
     @staticmethod
     def get_all_products():
         return Product.objects.all()
+
+    @staticmethod
+    def get_all_products_by_categoryid(category_id):
+        if category_id:
+            return Product.objects.filter(category=category_id)
+        else:
+            return Product.get_all_products();
--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views.signup import Signup
+from .views.login import Login
+from .views.home import Index
 
 urlpatterns = [
 
-    path('', index)
+    path('', Index.as_view(), name='homepage'),
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login')
 ]
